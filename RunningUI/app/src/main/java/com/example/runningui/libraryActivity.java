d4ae6e45386c8a961a9e59dec72fe5a421a53dc3
legocraftmation <danielnrose@yahoo.com>
2021-12-04 18:50:38 -0500

    Code

--- RunningUI/app/src/main/java/com/example/runningui/libraryActivity.java
+++ RunningUI/app/src/main/java/com/example/runningui/libraryActivity.java
@@ -3,13 +3,81 @@ package com.example.runningui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class libraryActivity extends AppCompatActivity {
+
+    private String filename = "";
+    private String filepath = "";
+    private String filecontent = "";
+    private FileReader fr;
+    private TextView load;
+
+    private Button clear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_library);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        filename = "pastruns.txt";
+        filepath = "MyFileDir";
+
+        load = (TextView) findViewById(R.id.load);
+        clear = (Button) findViewById(R.id.clear);
+
+        File externalFile = new File(getExternalFilesDir(filepath), filename);
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FileOutputStream fos = null;
+                try{
+                    fos = new FileOutputStream(externalFile);
+                    fos.write(filecontent.getBytes());
+                } catch (FileNotFoundException e){
+                    e.printStackTrace();
+
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+       // File externalFile = new File(getExternalFilesDir(filepath), filename);
+        try {
+            fr = new FileReader(externalFile);
+            BufferedReader bf = new BufferedReader(fr);
+            String line = bf.readLine();
+            while(line != null){
+                stringBuilder.append(line).append('\n');
+                line = bf.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+
+            String filecontents = stringBuilder.toString();
+            load.setText(filecontents);
+        }
+
+
     }
 }
--- RunningUI/app/src/main/java/com/example/runningui/settingsActivity.java
+++ RunningUI/app/src/main/java/com/example/runningui/settingsActivity.java
@@ -2,14 +2,53 @@ package com.example.runningui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class settingsActivity extends AppCompatActivity {
+
+    private String mb = "false";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        Switch s = (Switch) findViewById(R.id.bpmswitch);
+
+        s.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    mb = "true";
+                } else {
+                    mb = "false";
+                }
+            }
+        });
+
+        Button savebutton = (Button) findViewById(R.id.save);
+        savebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(settingsActivity.this, MainActivity.class);
+                EditText mEdit   = (EditText)findViewById(R.id.typedbpm);
+                String t = mEdit.getText().toString();
+                intent.putExtra("state", (String) mb);
+                intent.putExtra("bpm", t);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
 }
+
